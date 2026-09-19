fdd5f843d22e3d393111106129dc45bcdcce12a8
Priyansu Samal <ps7340@srmist.edu.in>
2026-09-19 11:10:21 +0530

    Add Week 5 OOP assignment problems

---
+++ src/main/java/oop_fundamentals/assigment_problems/FantasyLeagueAutoDraftRankingEngine.java
@@ -0,0 +1,101 @@
+package oop_fundamentals.assigment_problems;
+
+import java.util.Arrays;
+
+class Player implements Comparable<Player> {
+
+    private String name;
+    private int matchesPlayed;
+    private double battingAverage;
+    private boolean injured;
+
+    public Player(String name, int matchesPlayed, double battingAverage, boolean injured) {
+        this.name = name;
+        this.matchesPlayed = matchesPlayed;
+        this.battingAverage = battingAverage;
+        this.injured = injured;
+    }
+
+    static boolean isDraftable(int matchesPlayed) {
+        return matchesPlayed >= 10;
+    }
+
+    static boolean isDraftable(int matchesPlayed, boolean injured) {
+        return matchesPlayed >= 5 && !injured;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public double getBattingAverage() {
+        return battingAverage;
+    }
+
+    public boolean isInjured() {
+        return injured;
+    }
+
+    @Override
+    public int compareTo(Player other) {
+        return Double.compare(
+                other.getBattingAverage(),
+                this.getBattingAverage()
+        );
+    }
+}
+
+public class FantasyLeagueAutoDraftRankingEngine {
+
+    static String draftAndRank(Player[] players) {
+
+        Player[] draftable = new Player[players.length];
+        int count = 0;
+
+        for (Player player : players) {
+
+            if (Player.isDraftable(player.getMatchesPlayed())) {
+                draftable[count++] = player;
+
+            } else if (Player.isDraftable(
+                    player.getMatchesPlayed(),
+                    player.isInjured())) {
+
+                draftable[count++] = player;
+            }
+        }
+
+        draftable = Arrays.copyOf(draftable, count);
+
+        Arrays.sort(draftable);
+
+        String result = "";
+
+        for (int i = 0; i < draftable.length; i++) {
+
+            result += (i + 1) + ". " + draftable[i].getName();
+
+            if (i < draftable.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Player[] players = {
+            new Player("Virat", 15, 48.0, false),
+            new Player("Rahul", 7, 55.0, false),
+            new Player("Sameer", 3, 60.0, false),
+            new Player("Dev", 12, 20.0, true)
+        };
+
+        System.out.println(draftAndRank(players));
+    }
+}
