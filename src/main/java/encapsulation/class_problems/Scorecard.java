e037d57b9375b1392bc4596241ce661074d9d5cd
Priyansu Samal <ps7340@srmist.edu.in>
2026-09-19 11:53:13 +0530

    Add Week 7 encapsulation class problems

---
+++ src/main/java/encapsulation/class_problems/Scorecard.java
@@ -0,0 +1,43 @@
+package encapsulation.class_problems;
+
+public class Scorecard {
+
+    private boolean[] results;
+    private int recordedAnswers;
+
+    public Scorecard(int questionCount) {
+        results = new boolean[questionCount];
+        recordedAnswers = 0;
+    }
+
+    public void recordAnswer(boolean correct) {
+        if (recordedAnswers < results.length) {
+            results[recordedAnswers] = correct;
+            recordedAnswers++;
+        }
+    }
+
+    public int getScore() {
+        int score = 0;
+
+        for (int i = 0; i < recordedAnswers; i++) {
+            if (results[i]) {
+                score++;
+            }
+        }
+
+        return score;
+    }
+
+    public static void main(String[] args) {
+
+        Scorecard sc = new Scorecard(4);
+
+        sc.recordAnswer(true);
+        sc.recordAnswer(true);
+        sc.recordAnswer(false);
+        sc.recordAnswer(true);
+
+        System.out.println("Score: " + sc.getScore());
+    }
+}
